d6471a916daf3bbb1354b301de75e785e8e36655
DmutroZel <156704336+DmutroZel@users.noreply.github.com>
2024-06-24 12:25:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+$('.asistantPreview').hide(0);
+
+$('.messageP').hide(0);
+
+$('.asistantButton').click(function () {
+    $('.asistantPreview').slideToggle(300);
+})
+
+$('#send').click(function () {
+
+
+    $('.messageContainer').append(`<div class='messageItem'>${$('#message').val()}
+    <span>${new Date().getHours()}:${new Date().getMinutes()}</span>
+    </div>`);
+
+    
+})
+
+// let currentTime = new Date();
+
+// console.log(currentTime);]
+
+
+$('#speak').click(function () {
+  $('.asistantPreview').slideUp(500); 
+  $('.messageP').fadeToggle(500);
+})
+
+$('#tg').click(function () {
+    $('.asistantPreview').slideUp(500); 
+    window.location.href = 'https://creator-it-academy.com/';
+})
+
+$('#inst').click(function () {
+    $('.asistantPreview').slideUp(500); 
+    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+})
+$('#gt').click(function () {
+    $('.asistantPreview').slideUp(500); 
+    window.location.href = 'https://github.com/DmutroZel/asistant';
+})
